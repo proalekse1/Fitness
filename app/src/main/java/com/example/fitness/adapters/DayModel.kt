6b38777b71d094f2d16bee520e026e2d0fbc5f55
proalekse1 <proaleekse1@yandex.ru>
2022-12-26 10:22:23 +0300

    Day model comment and ExerciseModel

--- app/src/main/java/com/example/fitness/adapters/DayModel.kt
+++ app/src/main/java/com/example/fitness/adapters/DayModel.kt
@@ -1,7 +1,7 @@
 package com.example.fitness.adapters
 
 data class DayModel(
-    var exercises: String, // переменная для упражнений из массива
-    var dayNumber: Int, // переменная для 24 урока в которой передается во фрагмент со списком упражнений какой сейчас день
-    var isDone: Boolean // переменная для чекбокса
+    var exercises: String, //  переменная для упражнений из массива
+    var dayNumber: Int, //  переменная для 24 урока в которой передается во фрагмент со списком упражнений какой сейчас день
+    var isDone: Boolean //  переменная для чекбокса
 )
